a8b48894af3414549ff3ac9b655390f5331c394f
azyasaxi <124503738+wujiafanihao@users.noreply.github.com>
2024-06-25 13:46:06 +0800

    Delete Authorized directory

--- Authorized/authorized.py
+++
@@ -1,7 +0,0 @@
-from fastapi import HTTPException
-
-API_KEY = "Wjf251605@"
-
-def verify_api_key(api_key: str):
-    if api_key != API_KEY:
-        raise HTTPException(status_code=403, detail="Unauthorized")
